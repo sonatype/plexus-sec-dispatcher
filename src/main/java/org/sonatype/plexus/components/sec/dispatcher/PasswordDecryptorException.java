9af9727f979a3d236fab52c384ade44fb46db891
Josh Soref <jsoref@users.noreply.github.com>
2017-01-29 16:05:35 +0000

    spelling: decrypter

--- src/main/java/org/sonatype/plexus/components/sec/dispatcher/PasswordDecryptorException.java
+++ src/main/java/org/sonatype/plexus/components/sec/dispatcher/PasswordDecryptorException.java
@@ -1,63 +1,11 @@
-/**
- * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
- *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
- */
- 
 package org.sonatype.plexus.components.sec.dispatcher;
 
+import org.sonatype.plexus.components.sec.dispatcher.PasswordDecrypterException;
+
 /**
- *
- *
- * @author Oleg Gusakov
- * @version $Id$
- *
+ * @deprecated instead use {@link PasswordDecrypterException}
  */
-public class PasswordDecryptorException
-    extends Exception
+@Deprecated
+public class PasswordDecryptorException : PasswordDecrypterException
 {
-
-    /**
-     * 
-     */
-    public PasswordDecryptorException()
-    {
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @param message
-     */
-    public PasswordDecryptorException( String message )
-    {
-        super( message );
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @param cause
-     */
-    public PasswordDecryptorException( Throwable cause )
-    {
-        super( cause );
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @param message
-     * @param cause
-     */
-    public PasswordDecryptorException( String message, Throwable cause )
-    {
-        super( message, cause );
-        // TODO Auto-generated constructor stub
-    }
-
 }
